716ac856f5ad3d2cd862ef582730b91e447f0e0d
Matias Aliaga <112192212+Matiglorioso@users.noreply.github.com>
2022-11-02 11:45:54 -0300

    Add files via upload

---
+++ SP2xResolver/SP2xResolver/frmMain.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace SP2xResolver
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDistancia = new System.Windows.Forms.TextBox();
+            this.cmbDias = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDistanciaTotal = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtImporte = new System.Windows.Forms.TextBox();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Distancia [km]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dias";
+            // 
+            // txtDistancia
+            // 
+            this.txtDistancia.Location = new System.Drawing.Point(121, 34);
+            this.txtDistancia.Name = "txtDistancia";
+            this.txtDistancia.Size = new System.Drawing.Size(100, 20);
+            this.txtDistancia.TabIndex = 2;
+            // 
+            // cmbDias
+            // 
+            this.cmbDias.FormattingEnabled = true;
+            this.cmbDias.Location = new System.Drawing.Point(121, 92);
+            this.cmbDias.Name = "cmbDias";
+            this.cmbDias.Size = new System.Drawing.Size(100, 21);
+            this.cmbDias.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Distancia Total (ida y vuelta)";
+            // 
+            // txtDistanciaTotal
+            // 
+            this.txtDistanciaTotal.Location = new System.Drawing.Point(431, 34);
+            this.txtDistanciaTotal.Name = "txtDistanciaTotal";
+            this.txtDistanciaTotal.ReadOnly = true;
+            this.txtDistanciaTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtDistanciaTotal.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(567, 34);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(97, 40);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Costo Total";
+            // 
+            // txtImporte
+            // 
+            this.txtImporte.Location = new System.Drawing.Point(121, 159);
+            this.txtImporte.Name = "txtImporte";
+            this.txtImporte.ReadOnly = true;
+            this.txtImporte.Size = new System.Drawing.Size(100, 20);
+            this.txtImporte.TabIndex = 8;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(567, 95);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(97, 40);
+            this.btnCerrar.TabIndex = 9;
+            this.btnCerrar.Text = "&Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 262);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.txtImporte);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtDistanciaTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbDias);
+            this.Controls.Add(this.txtDistancia);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Organizaciòn Ferro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDistancia;
+        private System.Windows.Forms.ComboBox cmbDias;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDistanciaTotal;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtImporte;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
+
---
+++ SP2xResolver/SP2xResolver/frmMain.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SP2xResolver
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // estado inicial del formulario
+            int Dia; // variable para cargar el combo de dias
+            txtDistancia.Text = "";
+            txtDistanciaTotal.Text = "";
+            txtImporte.Text = "";
+            cmbDias.Items.Clear();
+            for(Dia = 1; Dia <= 30; Dia++) // se carga con los días 1 a 30
+            {
+                cmbDias.Items.Add(Dia);
+            }
+            cmbDias.SelectedIndex = 0;
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            float Importe = 0;
+            int Distancia = 0;
+            int Dias;
+
+            Dias = int.Parse(cmbDias.Text);
+            Distancia = int.Parse ( txtDistancia.Text );
+            Distancia = Distancia * 2;
+            txtDistanciaTotal.Text = Distancia.ToString();
+            Importe = Distancia * 5;
+            if(Dias >= 7 && Distancia >= 100)
+            {
+                Importe = Importe * 0.5f; // se agrega la letra "f" para indicar que el valor se tome de tipo float
+            }
+            txtImporte.Text = Importe.ToString();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
